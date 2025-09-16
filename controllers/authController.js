fdaf4ee7900e73ff1308cae104ae43cc20cfa0b5
Suyash <suyashrajsingh424@gmail.com>
2025-09-16 16:01:48 +0530

    Commiting changes for day 2 branch

---
+++ controllers/authController.js
@@ -0,0 +1,58 @@
+import bcrypt from 'bcrypt';
+import {User} from '../models/User.js';
+
+export const registerUser = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        //Basic validation : ->
+        if (!username || !email || !password) {
+            return res.status(400).json({
+                message: "(inside authController, basic vali..) All fields are required..."
+            })
+        }
+
+        const existingUser = await User.findOne({ $or: [{ email }, { username }] });
+
+        if (existingUser) {
+            return res.status(400).json({
+                message: "User already exists.."
+            })
+        }
+
+        //Hash Password: ->
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        //Create new User : ->
+        const user = new User(
+            {
+                username,
+                email,
+                password
+            }
+        );
+
+        await user.save();
+
+        //Exclude password from response: ->
+        const userData = {};
+        const userObject = user.toObject();
+
+        for (const key in userObject) {
+            if (key !== 'password') {
+                userData[key] = userObject[key];
+            }
+        }
+
+        return res.status(200).json({
+            message: "User successfully registered...",
+            user: userData,
+        });
+
+    } catch (error) {
+        console.log("(inside /controllers/authControllers) Registration error: -> ", error);
+        res.status(500).json({
+            message: "Server Error..."
+        })
+    }
+}
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import connectDB from './config/db.js';
-import healthRoute from './routes/health.js'
+import healthRoute from './routes/health.js';
+import authRoute from './routes/auth.js';
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 
 //routes: ->
 app.use('/api/health', healthRoute);
+app.use('/api/auth', authRoute);
 //connect DB and start server: ->
 const PORT = process.env.PORT;
 connectDB();
