981818e0f7aa178411bde8e57238136a1b017548
Suyash <suyashrajsingh424@gmail.com>
2025-09-16 16:19:11 +0530

    first commit of day-3 branch

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,7 +28,7 @@ export const registerUser = async (req, res) => {
             {
                 username,
                 email,
-                password
+                password: hashedPassword,
             }
         );
 
